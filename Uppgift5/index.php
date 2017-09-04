044657f00192fb00c8a5c7e651e3d892c04022e1
Erik Tedfelt Lennström <erik@tedfeltlennstrom.se>
2017-09-04 20:01:20 +0200

    Uppgift 5 finished

--- Uppgift5/index.php
+++ Uppgift5/index.php
@@ -47,26 +47,38 @@
 		]);
 		//var_dump($form);
 		$form->validate();
-		//var_dump($form->errors[0]);
+
+		function in_array_r($needle, $haystack, $strict = false) {
+		    foreach ($haystack as $item) {
+		        if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+		            return true;
+		        }
+		    }
+		    return false;
+		}
 	}
  ?>
 <body>
+	<?php
+	if(!empty($form->errors)){ ?>
 	<h3>Fält saknas</h3>
 	<ul>
 		<?php
-		for ($i = 0; $i < count($form->errors); $i++)  {
-			//var_dump();
-			echo '<li>Fältet ' . $form->errors[$i]['name'] . ' är fel. Meddelande: ' . $form->errors[$i]['message'] . '</li>';
+			for ($i = 0; $i < count($form->errors); $i++)  {
+				//var_dump();
+				echo '<li>Fältet ' . $form->errors[$i]['name'] . ' är fel. Meddelande: ' . $form->errors[$i]['message'] . '</li>';
+			}
 		}
+		if(count($form->errors) == 0) echo 'Tack ' . $_POST['surname'];
 		?>
 	</ul>
 	<form action="index.php" method="post">
-	    <input type="text" name="firstname" placeholder="Förnamn" class="<?php echo (!in_array('firstname', $form->errors) && !isset($_POST['submit'])) ? 'form-valid' : 'form-error'; ?>"><br>
-	    <input type="text" name="surname" placeholder="Efternamn" class="<?php echo (!in_array('surname', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
-	    <input type="email" name="email" placeholder="Email" class="<?php echo (!in_array('email', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
-	    <input type="tel" name="tel" placeholder="Telephone" class="<?php echo (!in_array('tel', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
-	    <input type="password" name="password" placeholder="Password" class="<?php echo (!in_array('password', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
-	    <input type="password" name="passwordConfirm" placeholder="Password confirmation" class="<?php echo (!in_array('password', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
+	    <input type="text" name="firstname" placeholder="Förnamn" class="<?php echo (!in_array_r('firstname', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
+	    <input type="text" name="surname" placeholder="Efternamn" class="<?php echo (!in_array_r('surname', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
+	    <input type="email" name="email" placeholder="Email" class="<?php echo (!in_array_r('email', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
+	    <input type="tel" name="tel" placeholder="Telephone" class="<?php echo (!in_array_r('tel', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
+	    <input type="password" name="password" placeholder="Password" class="<?php echo (!in_array_r('password', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
+	    <input type="password" name="passwordConfirm" placeholder="Password confirmation" class="<?php echo (!in_array_r('password', $form->errors)) ? 'form-valid' : 'form-error'; ?>"><br>
 	    <input type="submit" name="submit" value="Skicka in">
 	</form>
 </body>
